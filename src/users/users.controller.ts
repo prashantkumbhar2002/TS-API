cba9c0194c08726594cc53c22057de381eb6bd8f
prashantkumbhar2002 <prashantkumbhar5515@gmail.com>
2024-07-02 11:58:48 +0530

    Added proper error handling for the controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,7 @@ import userModel from "./users.model";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { config } from "../config/config";
+import { User } from "./user.types";
 
 const registerUsers = async (req: Request, res: Response, next: NextFunction) => {
     const { name, email, password } = req.body;
@@ -11,24 +12,34 @@ const registerUsers = async (req: Request, res: Response, next: NextFunction) =>
         const error = createHttpError(400, "All fields are required");
         return next(error);
     }
-    const user = await userModel.findOne({ email });
-    if(user){
-        const error = createHttpError(400, "User already exists with this email");
-        return next(error);
+
+    try {
+        const user = await userModel.findOne({ email });
+        if(user){
+            const error = createHttpError(400, "User already exists with this email");
+            return next(error);
+        }
+    } catch (error) {
+        return next(createHttpError(500, 'Error while fetching the user'))
     }
 
     //password hashing
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await userModel.create({
-        name,
-        email,
-        password: hashedPassword
-    });
-
-    if(!newUser){
-        const error = createHttpError(500, 'Error while saving details of User');
-        return next(error);
+    let newUser: User;
+    try {
+        newUser = await userModel.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+    } catch (error) {
+        return next(createHttpError(500, 'Error while creating new User'))
     }
+
+    // if(!newUser){
+    //     const error = createHttpError(500, 'Error while saving details of User');
+    //     return next(error);
+    // }
 
     //token generation
     const token = jwt.sign( {sub: newUser._id }, config.jwtSecret as string, { expiresIn: config.jwtExpiry})
